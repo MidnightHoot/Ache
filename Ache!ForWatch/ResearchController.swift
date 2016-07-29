f23aa59adb6bc12c363fad0560e0487f83c33d29
Tomek_L <Tomek@longfeideMacBook-Pro.local>
2016-07-30 02:00:42 +0800

    Watch
    只是备份一下

---
+++ Ache!ForWatch/ResearchController.swift
@@ -0,0 +1,17 @@
+//
+//  ResearchController.swift
+//  Ache!
+//
+//  Created by 龙飞 on 16/7/24.
+//  Copyright © 2016年 MidnightHoot. All rights reserved.
+//
+
+import Foundation
+import WatchKit
+import UIKit
+
+class ResearchController:WKInterfaceController
+{
+    @IBOutlet var DiseaseIdentifier: WKInterfaceLabel!
+    
+}
